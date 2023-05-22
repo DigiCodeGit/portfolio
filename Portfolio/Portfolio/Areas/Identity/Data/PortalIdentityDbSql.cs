32d561f6a8003903e6639afb02f30b42d6119775
bluegit2015 <blue_sky_escape@protonmail.com>
2023-05-21 23:19:34 -0700

    Working Files
    Previous files can't run (missing files)

---
+++ Portfolio/Portfolio/Areas/Identity/Data/PortalIdentityDbSql.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Portfolio.Areas.Identity.Data;
+
+namespace Portfolio.Areas.Identity.Data;
+
+public class PortalIdentityDbSql : IdentityDbContext<PortalIdentityUsers>
+{
+    public PortalIdentityDbSql(DbContextOptions<PortalIdentityDbSql> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
